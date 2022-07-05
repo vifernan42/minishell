a808ebf9de57fcdaaf3645791e6b72daedfccbe8
Inigo Alvarez echarri <ialvarez@c1r12s6.42madrid.com>
2022-07-05 21:23:22 +0200

    env-save

---
+++ env.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ialvarez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 20:09:52 by ialvarez          #+#    #+#             */
+/*   Updated: 2022/07/05 20:43:07 by ialvarez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	**keep_env(char **env)
+{
+	char	**envir;
+	int		i;
+	int		j;
+
+	i = 0;
+	while(env[i] != NULL)
+		i++;
+	envir = (char **)malloc((i + 1) * sizeof(char *));
+	i = 0;
+	while (env[i] != NULL)
+	{
+		j = 0;
+		envir[i] = (char *)malloc((ft_strlen(envir[i]) + 1) * sizeof(char));
+		while (env[i][j])
+		{
+			envir[i][j] = env[i][j];
+			j++;
+		}
+		envir[i][j] = '\0';
+		i++;
+	}
+	envir[i] = NULL;
+	return (envir);
+}
