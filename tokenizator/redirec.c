21b8bbbe0665ff844be19841fdfce0fe249eba67
cacharri <ialvarez@student.42madrid.com>
2022-08-09 13:51:46 +0200

    parse_redirecction

--- tokenizator/redirec.c
+++ tokenizator/redirec.c
@@ -6,7 +6,7 @@
 /*   By: ialvarez <ialvarez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 14:08:38 by vifernan          #+#    #+#             */
-/*   Updated: 2022/08/08 21:17:31 by ialvarez         ###   ########.fr       */
+/*   Updated: 2022/08/09 13:12:59 by ialvarez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,32 @@
 char	*take_fname(char **cmd_sp, int i, int x, int *join)
 {
 	char	*fname;
-	char *aux = NULL;
-	int o = -1;
+	char	*aux;
+	int		o;
 	int		flag;
 	
 	flag = -1;
+	o = -1;
 	fname = NULL;
+	aux = NULL;
 	if ((int)ft_strlen(cmd_sp[i]) > x) /* >a>b return: a */
 	{
 		if (ft_strnstr(cmd_sp[i], ">>", 2))
 		{
-			fname = ft_substr(cmd_sp[i], 2, ft_strlen(cmd_sp[i]) - 2);
-			//printf("----  %s\n", cmd_sp[i]);	
+			aux = ft_strchr(cmd_sp[i], '>') + 2; /*me da a mi que esto no*/
+			while (aux[++o] != '\0')
+				if (aux[o] == '>' || aux[o] == '<')
+					break ;
+			fname = ft_substr(aux, 0, ft_strlen(aux) - (ft_strlen(aux) - o));
+			/*fname = ft_substr(cmd_sp[i], 2, ft_strlen(cmd_sp[i]) - 2);*/
+			printf("----  %s\n", cmd_sp[i]);	
 		}
 		else if(ft_str2chr(cmd_sp[i], '>'))
 		{
+			o = -1;
 			aux = ft_strchr(cmd_sp[i], '>') + 1;
 			while (aux[++o] != '\0')
-				if (aux[o] == '>' || aux[o] == '<' || aux[o] == '|')
+				if (aux[o] == '>' || aux[o] == '<')
 					break ;
 			fname = ft_substr(aux, 0, ft_strlen(aux) - (ft_strlen(aux) - o));
 		}
