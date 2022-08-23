f0498f2f243269cb80b849765e9b1ffdbe160fff
vifernan42 <vifernan@student.42madrid.com>
2022-08-23 16:46:40 +0200

    lets_pipe

--- exec/exec.c
+++ exec/exec.c
@@ -6,11 +6,17 @@
 /*   By: vifernan <vifernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 17:03:56 by vifernan          #+#    #+#             */
-/*   Updated: 2022/08/18 17:39:26 by vifernan         ###   ########.fr       */
+/*   Updated: 2022/08/23 16:45:52 by vifernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	lets_pipe(t_pipe *pipe)
+{
+	int	pid;
+	pid = fork();
+}
 
 void	exec_pipes(t_pipe *pipe)
 {
