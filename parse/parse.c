d1cad4c813905b4a62deaf184dc78fad99a9e06c
vifernan42 <vifernan@student.42madrid.com>
2023-01-17 19:50:19 +0100

    double-expand_KO

--- parse/parse.c
+++ parse/parse.c
@@ -6,69 +6,65 @@
 /*   By: vifernan <vifernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 20:06:04 by vifernan          #+#    #+#             */
-/*   Updated: 2023/01/16 20:51:06 by vifernan         ###   ########.fr       */
+/*   Updated: 2023/01/17 19:49:41 by vifernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	change_value(char *str, int i, t_data *data, int index)
+int	change_value(char *str, int i, t_data *data, char **expand_ln)
 {
 	char	*var_name;
 	int		char_index;
 	char	*var_env;
+	char	*aux;
 	int		leng;
 	
+	printf("-	%s\n", str + i);
 	leng = 0;
 	data->err = 0;
 	char_index = ft_strinkey(str + i, "$,. ><-@?¿¡/\\%#·\"\'");
 	leng = (int)ft_strlen(str);
-	printf("leng--%d\n", leng);
-	printf("-%s$\n", ft_substr(str + i, 0, char_index));
-	
 	var_name = ft_substr(str + i, 0, char_index);
 	var_env = ft_strjoin(var_name, "=");
 	free(var_name);
 	var_name = search_variable(data->env, var_env);
 	free(var_env);
-	printf("-	%s\n", var_name);
-	index = 0;
-	if (var_name)
+	if (!var_name)
 	{
-		var_env = ft_strdup(str);
-		//free(str);
-		//printf("&	%s\n", ft_substr(str, 0, i - 1));
-		str = ft_strjoin(ft_substr(var_env, 0, i - 1), var_name);
-		printf("·	%s\n", str);
-		printf("+	%s\n", var_env);
-		var_env = ft_strjoin(str, ft_substr(var_env, (i + char_index), leng - (i + char_index)));
-		str = var_env;
-		//data->spt_pipes[index] = var_env;
-//		free(str);
-		free(var_env);
-		
-		
-		//var_env = ft_substr(var_env, i + (int)ft_strlen(var_name) + (int)ft_strlen(str), ((int)ft_strlen(var_env) - (i + (int)ft_strlen(var_name)) + (int)ft_strlen(str)));
+		free(var_name);
+		var_name = "";
 	}
-	printf("+	%s\n", var_env);
-		/* cambiar por var_env */
-	//else
-		/* cambiar por "" */
-	//free(var_name);
-	//str = new_str;
-	return (i + (int)ft_strlen(var_name));
+	var_env = ft_strdup(str);
+	free(str);
+	aux = ft_substr(var_env, 0, i - 1);
+	str = ft_strjoin(aux, var_name);
+	free(aux);
+	if (char_index > 0)
+	{
+		aux = ft_substr(var_env, (i + char_index), leng - (i + char_index));
+		*expand_ln = ft_strjoin(str, aux);
+		free(aux);
+	}
+	else
+		*expand_ln = ft_strdup(str);
+	free(var_env);
+	return (i + char_index);
 }
 
-void	take_variable(t_data *data, char *str, int index)
+char	*take_variable(t_data *data, char *str)
 {
 	int	i;
 	int	open;
+	char *expand_ln;
 	
 	i = -1;
 	data->err = 0;
 	open = 0;
+	expand_ln = NULL;
 	while (str[++i] != '\0')
 	{
+		printf("_str[%d]->%c\n", i, str[i]);
 		if (str[i] == '\'')
 		{
 			if (open)
@@ -77,21 +73,32 @@ void	take_variable(t_data *data, char *str, int index)
 				open = 1;
 		}
 		if (str[i] == '$' && !open)
-			i = change_value(str, i + 1, data, index);
+		{
+			printf("-str[%d]->%c\n", i, str[i]);
+			i = change_value(ft_strdup(str), i + 1, data, &expand_ln);
+			printf("%s\n=str[%d]->%c\n", str, i, str[i]);
+		}
 	}
+	if (!expand_ln)
+		free(str);
+	return (expand_ln);
 }
 
 int	more_redir(t_data *data, int i, int j, char **aux)
 {
 	int	found;
+	char	*expand_ln;
 
 	found = 0;
 	while (data->spt_pipes[++i] != NULL)
 	{
-		take_variable(data, data->spt_pipes[i], i);
-		printf("*	%s\n", data->spt_pipes[i]);
+		expand_ln = take_variable(data, ft_strdup(data->spt_pipes[i]));
+		if (expand_ln)
+		{
+			free(data->spt_pipes[i]);
+			data->spt_pipes[i] = expand_ln;	
+		}
 		aux = cmd_arg_quottes(data->spt_pipes[i], data);
-		
 		j = -1;
 		while (aux[++j] != NULL)
 		{
